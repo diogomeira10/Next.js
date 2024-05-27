92ccc986eff6ed38e76ea9f7579ab4636f28b721
Diogo Meira <diogomeira81@gmail.com>
2024-05-27 18:00:51 +0100

    PATCH Requests

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,14 +1,25 @@
 import { comments } from "../data"
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+// export async function GET(request: Request, { params }: { params: { id: string } }) {
 
-    const comment = comments.find(comment => comment.id === parseInt(params.id))
+//     const comment = comments.find(comment => comment.id === parseInt(params.id))
 
-    return Response.json(comment)
-}
-
-// export async function PATCH({ params }:
-//     { params: { id: string } }
-// ) {
-//     return ''
+//     return Response.json(comment)
 // }
+
+export async function PATCH(request: Request, { params }:
+    { params: { id: string } }
+) {
+
+    const body = await request.json()
+
+    const { text } = body
+
+    const index = comments.findIndex((comment) => {
+        return comment.id === parseInt(params.id)
+    })
+
+    comments[index].text = text
+
+    return Response.json(comments[index])
+}
