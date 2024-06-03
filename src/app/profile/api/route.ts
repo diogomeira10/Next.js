5a8b2b413e3fef2b6be273bd6be2dbc910d56f60
Diogo Meira <diogomeira81@gmail.com>
2024-06-03 09:00:42 +0100

    Headers in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,18 @@
-export async function GET() {
-    return new Response('Hello from Profile Page')
+import { type NextRequest } from "next/server"
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+
+    const requestHeaders = new Headers(request.headers)
+
+    const headerList = headers()
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"));
+
+    return new Response('<h1>Profile API Data</h1>', {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    })
 }
+
